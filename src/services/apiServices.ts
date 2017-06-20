c5918493b5241f85c52cbd09aa7e53945df8d606
Craig Doremus <cdoremus@gmail.com>
2017-06-20 17:28:29 -0400

    Fixed stupid mistake! I guess hard-code 'address' string is equivalent to some place in Rhode Island! :)

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -2,5 +2,5 @@ import axios, {AxiosResponse} from 'axios';
 import * as Constants from '../common/constants';
 
 export const getIssues = (address: string): Promise<AxiosResponse> => {
-  return axios.get(`${Constants.ISSUES_API_URL}address`);
+  return axios.get(`${Constants.ISSUES_API_URL}${address}`);
 };
