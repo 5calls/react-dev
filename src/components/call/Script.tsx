52dacf904514ea373078cb6144fd21113c3a1ef3
Erin Stanfill & Laura Little (lnathanson) <erin.stanfill@versionone.com>
2017-08-25 15:20:41 -0700

    #20 Script uses current contact to personalize script text

--- src/components/call/Script.tsx
+++ src/components/call/Script.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { TranslationFunction } from 'i18next';
 import { translate } from 'react-i18next';
-import { Issue } from '../../common/model';
+import {Contact, Issue} from '../../common/model';
 import { LocationState } from '../../redux/location/reducer';
 
 interface Props {
@@ -12,14 +12,24 @@ interface Props {
 }
 
 // Replacement regexes, ideally standardize copy to avoid complex regexs
-// const titleReg = /\[REP\/SEN NAME\]|\[SENATOR\/REP NAME\]/gi;
+const titleReg = /\[REP\/SEN NAME\]|\[SENATOR\/REP NAME\]/gi;
 const locationReg = /\[CITY,\s?ZIP\]|\[CITY,\s?STATE\]/gi;
 
-function scriptFormat(issue: Issue, locationState: LocationState) {
+function getContactNameWithTitle(contacts: Contact[], contactIndex: number) {
+    const currentContact = contacts[contactIndex];
+    const title = currentContact.area === 'House' ? 'Rep. ' : 'Senator ';
+    return title + currentContact.name;
+}
+
+function scriptFormat(issue: Issue, locationState: LocationState, contactIndex: number) {
     const location = locationState.cachedCity;
     let script = issue.script;
     if (location) {
          script = script.replace(locationReg, location);
+    }
+    if (issue.contacts) {
+        const title = getContactNameWithTitle(issue.contacts, contactIndex);
+        script = script.replace(titleReg, title);
     }
     return script;
 }
@@ -30,7 +40,7 @@ const Script: React.StatelessComponent<Props> = ({ issue, contactIndex = 0, loca
       <div className="call__script">
         <h3 className="call__script__header">{t('script.yourScript')}</h3>
         <div className="call__script__body">
-          {scriptFormat(issue, locationState).split('\n').map((line, index) =>
+          {scriptFormat(issue, locationState, contactIndex).split('\n').map((line, index) =>
           <p key={index}>{line}</p>
           )}
           {/* issuesLink(state, prev, send) */}
