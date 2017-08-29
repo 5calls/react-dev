20430cbefc121de9f84ca35c32922657a7dbf332
Craig Doremus <cdoremus@gmail.com>
2017-08-29 17:38:31 -0400

    Switched Constants.APP_URL to 5calls.org

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,17 +1,9 @@
 
 export const APP_NAME = '5 Calls';
-<<<<<<< a26b5303687b205b71f998717e7ec521b45788c1
 export const APP_URL = 'https://5calls.org';
-export const API_URL = 'https://api.5calls.org/v1';
-export const ISSUES_API_URL = `${APP_URL}/issues/?address=`;
-export const REGISTER_CALL_API_URL = `${APP_URL}/report`;
-export const REPORT_API_URL = `${API_URL}/counts`;
-=======
-// export const APP_URL = 'https://5calls.org';
-export const APP_URL = 'http://localhost:8090';
+// export const APP_URL = 'http://localhost:8090';
 export const ISSUES_API_URL = `${APP_URL}/issues/?all=true&address=`;
 export const REPORT_API_URL = `${APP_URL}/report`;
->>>>>>> Some refactoring and almost done with the more issues page.
 export const IP_INFO_URL = 'https://ipinfo.io/json';
 
 export const zipCodeRegex: RegExp = /^\d{5}(-\d{4})?$/;
