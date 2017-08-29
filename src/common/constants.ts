98b5437035a99e54bd9b93779e7a299a094e97e8
Craig Doremus <cdoremus@gmail.com>
2017-08-29 18:06:39 -0400

    Fixed URLs mixed up during rebase conflict resolution.

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,8 +2,10 @@
 export const APP_NAME = '5 Calls';
 export const APP_URL = 'https://5calls.org';
 // export const APP_URL = 'http://localhost:8090';
+export const API_URL = 'https://api.5calls.org/v1';
 export const ISSUES_API_URL = `${APP_URL}/issues/?all=true&address=`;
-export const REPORT_API_URL = `${APP_URL}/report`;
+export const REGISTER_CALL_API_URL = `${APP_URL}/report`;
+export const REPORT_API_URL = `${API_URL}/counts`;
 export const IP_INFO_URL = 'https://ipinfo.io/json';
 
 export const zipCodeRegex: RegExp = /^\d{5}(-\d{4})?$/;
