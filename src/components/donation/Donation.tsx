5e3ce9f1bc965b1af28201981ff78552303eff23
Craig Doremus <cdoremus@gmail.com>
2017-08-31 16:23:01 -0400

    Does not show component if donations and goal are zero

--- src/components/donation/Donation.tsx
+++ src/components/donation/Donation.tsx
@@ -6,20 +6,28 @@ interface Props {
   goal: number;
 }
 
-export const Donation: React.StatelessComponent<Props> = (props: Props) => {
-  const pctDone = (props.total / props.goal) * 100;
-  const pctDoneStyle = {width: `${pctDone}%`};
-  return (
-    <div className="logo__header__inner layout">
-      <div className="logo__header__donatebar">
-        <span style={pctDoneStyle} className="logo__header__donatebar__total">
-            {`\$${formatNumber(props.total)}`}
-        </span>
-        <span className="logo__header__donatebar__goal">{`\$${formatNumber(props.goal)}`}</span>
-      </div>
-        <p className="logo__header__donatetext">
-          <a href="https://secure.actblue.com/donate/5calls-donate">Donate today to keep 5 Calls running</a>
-        </p>
-    </div>
-  );
-};
+interface State {}
+
+export class Donation extends React.Component<Props, State> {
+  render () {
+    // if goal is zero, percent = 100 to avoid divide by zero error
+    const pctDone = this.props.goal && this.props.goal !== 0 ? (this.props.total / this.props.goal) * 100 : 100;
+    const pctDoneStyle = {width: `${pctDone}%`};
+    if (this.props.goal !== 0 && this.props.total !== 0) {
+      return (
+        <div className="logo__header__inner layout">
+          <div className="logo__header__donatebar">
+            <span style={pctDoneStyle} className="logo__header__donatebar__total">
+                {`\$${formatNumber(this.props.total)}`}
+            </span>
+            <span className="logo__header__donatebar__goal">{`\$${formatNumber(this.props.goal)}`}</span>
+          </div>
+          <p className="logo__header__donatetext">
+            <a href="https://secure.actblue.com/donate/5calls-donate">Donate today to keep 5 Calls running</a>
+          </p>
+        </div>);
+    } else {
+      return <span/>;
+    }
+  }
+}
--- src/components/donation/DonationContainer.tsx
+++ src/components/donation/DonationContainer.tsx
@@ -12,7 +12,7 @@ interface StateProps {
 const mapStateToProps = (state: ApplicationState, ownProps: OwnProps): StateProps => {
   return {
     total: state.remoteDataState.donations ? state.remoteDataState.donations.total : 0,
-    goal: state.remoteDataState.donations ? state.remoteDataState.donations.amount : 1
+    goal: state.remoteDataState.donations ? state.remoteDataState.donations.amount : 0
   };
 };
 
