1dbf30d9eda85ace6bdc68b2135fa144d78c5486
Craig Doremus <cdoremus@gmail.com>
2017-08-31 13:46:45 -0400

    Removed stubbed out file

--- src/services/apiServices.test.ts
+++
@@ -1,8 +0,0 @@
-// import { getDonations } from './apiServices';
-
-test('getDonations', () => {
-  console.log('getDonations test');
-  // getDonations()
-  //   .then(actual => console.log('DONATIONS: ', actual))
-  //   .catch(e => console.error('ERROR', e));
-});
